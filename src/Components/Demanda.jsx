29641232f24c4f7167241b8c99fc7e0c2dbb6ed0
Hector Rincon <hecerinc@hotmail.com>
2018-05-26 18:50:53 -0500

    Add scraping current values to Demanda. Translate

--- src/Components/Demanda.jsx
+++ src/Components/Demanda.jsx
@@ -17,8 +17,14 @@ let dateOpts = {year: 'numeric', month: 'long', day: 'numeric' };
 const todayData = require('../data/todayDemand.json');
 
 const defaultData = todayData['SIN']['MONTERREY'];
-
+const zonenames = {
+	'SIN': 'Sistema Intercontectado Nacional',
+	'BCA': 'Baja California',
+	'BCS': 'Baja California Sur'
+};
 const baseUrl = process.env.NODE_ENV === "production" ? process.env['REACT_APP_OUTLOOK_PRODUCTION_SERVER_BASE_URL'] : process.env['REACT_APP_OUTLOOK_SERVER_BASE_URL'];
+
+// energia.org/outlook_server
 
 export default class Demanda extends React.Component {
 
@@ -26,14 +32,37 @@ export default class Demanda extends React.Component {
 		super();
 		this.state = {
 			selectedSystem: 'SIN',
-			selectedZone: null
+			selectedZone: null,
+			currentDemand: null,
 		};
 		this.loadChart = this.loadChart.bind(this);
 		this.selectZone = this.selectZone.bind(this);
 		this.selectFilterUpdated = this.selectFilterUpdated.bind(this);
+		this.updateCurrentDemand = this.updateCurrentDemand.bind(this);
 	}
 	componentDidMount() {
 		this.loadChart();
+
+		// Scrape the current demand
+
+		// 10: Sistema Intercontectado Nacional
+		// 1: Baja California
+		// 2: Baja California Sur
+
+		// URL: http://www.cenace.gob.mx/GraficaDemanda.aspx/obtieneValoresTotal
+		// Param name: gerencia
+		request(`${baseUrl}/demanda/current`, (error, response, body) => {
+			if(error){
+				alert('Could not retrieve current demand');
+				console.error(error);
+			}
+
+			let res = JSON.parse(body);
+			this.setState({currentDemand: res});
+		});
+	}
+	updateCurrentDemand(newVal) {
+
 	}
 	loadChart() {
 		this.chart = new Highcharts.Chart('chart', {
@@ -42,12 +71,12 @@ export default class Demanda extends React.Component {
 				backgroundColor : 'transparent'
 			},
 			title : {
-				text : 'Demanda pronosticada',
+				text : 'Assigned MW for DAM based on previous day figures',
 				style: {color: "#fff"}
 			},
 			subtitle : {
 				//text : ,
-				text : `MONTERREY  |  ${(new Date().toLocaleDateString('es-MX', dateOpts))}`,
+				text : `MONTERREY  |  ${(new Date().toLocaleDateString('en-US', dateOpts))}`,
 				style: {
 					color: "#fff"
 				}
@@ -56,7 +85,7 @@ export default class Demanda extends React.Component {
 				tickColor: '#1C324A',
 				tickInterval: 1,
 				title: {
-					text: 'Hora',
+					text: 'Hour',
 					style: {color: '#fff'}
 				},
 				type: 'linear',
@@ -145,7 +174,7 @@ export default class Demanda extends React.Component {
 				// FUTURE: Add real demand when data becomes available
 				// { name : 'Demanda real' },
 				{
-					name : 'Pronóstico MDA',
+					name : 'DAM Forecast',
 					type : "area",
 					// color: '#F3CF62',
 					color: '#4CF1CD',
@@ -173,7 +202,7 @@ export default class Demanda extends React.Component {
 			if(!error && response && response.statusCode == 200){
 				let res = JSON.parse(body);
 				this.chart.series[0].update({data: res.data}, false);
-				this.chart.setTitle(null, {text: `${zdc} | ${(new Date().toLocaleDateString('es-MX', dateOpts))}`});
+				this.chart.setTitle(null, {text: `${zdc} | ${(new Date().toLocaleDateString('en-US', dateOpts))}`});
 				this.chart.redraw();
 			}
 		});
@@ -194,10 +223,10 @@ export default class Demanda extends React.Component {
 			break;
 		}
 	}
-	shouldComponentUpdate(nextProps, nextState) {
-		// Means you're updating something else
-		return nextState.selectedSystem === this.state.selectedSystem;
-	}
+	// shouldComponentUpdate(nextProps, nextState) {
+	// 	// Means you're updating something else
+	// 	return nextState.selectedSystem === this.state.selectedSystem;
+	// }
 
 	render() {
 
@@ -208,8 +237,8 @@ export default class Demanda extends React.Component {
 			<div className={hideClass}>
 				<div className="row">
 					<div className="col titles">
-						<h2>Demanda pronosticada</h2>
-						<h3>{new Date().toLocaleDateString('es-mx', dateOpts)}</h3>
+						<h2>Assigned demand for DAM</h2>
+						<h3>{new Date().toLocaleDateString('en-US', dateOpts)}</h3>
 					</div>
 				</div>
 				<hr/>
@@ -222,16 +251,25 @@ export default class Demanda extends React.Component {
 							<table className="scalarTable">
 								<tbody>
 									<tr>
-										<td>
-											<p className="figure">35,560 <span className="units">MW</span></p>
-											<p className="desc">Demanda actual</p>
-										</td>
-										<td>
-											<p className="figure">29,095 <span className="units">MW</span></p>
-											<p className="desc small">Demanda m&aacute;xima pronosticada</p>
+										<td colSpan="2" style={{borderLeft: '0 none'}}>
+											{zonenames[this.state.selectedSystem]}
 										</td>
 									</tr>
-									<tr>
+									<tr style={{border: 0}}>
+										<td>
+											<p className="figure">
+												{this.state.currentDemand ? parseInt(this.state.currentDemand[this.state.selectedSystem][0]) : '-' } <span className="units">MW</span>
+											</p>
+											<p className="desc">Actual demand</p>
+										</td>
+										<td>
+											<p className="figure">
+												{this.state.currentDemand ? parseInt(this.state.currentDemand[this.state.selectedSystem][1]) : '-' } <span className="units">MW</span>
+												</p>
+											<p className="desc small">Max forecasted demand</p>
+										</td>
+									</tr>
+									{/*<tr>
 										<td>
 											<p className="figure">28,340 <span className="units">MW</span></p>
 											<p className="desc">Demanda m&aacute;xima</p>
@@ -241,7 +279,7 @@ export default class Demanda extends React.Component {
 											<p className="desc small">Demanda m&aacute;xima pronosticada para mañana</p>
 										</td>
 										
-									</tr>
+									</tr>*/}
 								</tbody>
 							</table>
 						</div>
