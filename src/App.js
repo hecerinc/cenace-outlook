3f11bf8a1140b8d88aa4c8832c1755f451237c2d
Hector Rincon <hecerinc@hotmail.com>
2018-05-26 18:46:47 -0500

    Add footer. Replace dummy text. Change title

--- src/App.js
+++ src/App.js
@@ -31,15 +31,16 @@ class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<h1 className="fw300" style={{marginBottom: 0}}>Sistema El&eacute;ctrico Nacional</h1>
-				<h3 style={{fontSize: '12px', marginTop: 0, color: '#537095'}}><span style={{fontSize: '10px'}}>una iniciativa de</span> Berkeley Energy and Climate Institute</h3>
+				<h1 className="fw300" style={{marginBottom: 0}}>Sistema El&eacute;ctrico Nacional / Mexico's Electricity System</h1>
+				<h3 style={{fontSize: '12px', marginTop: 0, color: '#537095'}}>a UC Berkeley California Institute of Energy and Environment initiative</h3>
 
-				<p className="firstp">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet molestiae consequuntur ducimus dicta explicabo reiciendis magni dolores tempore dolor enim nulla accusantium, ea velit, a aperiam. Facilis minus minima aperiam officiis ratione at similique perspiciatis fugit quas omnis, ipsum sit eligendi, nostrum autem nisi reiciendis repellendus, aliquam molestiae reprehenderit quasi quo impedit id maxime odit. Libero, reprehenderit. Iusto sit enim, fugit amet expedita, a optio illo molestiae rerum. Quos sed doloremque omnis dolorum similique nemo distinctio, perferendis veniam eos aperiam quo quibusdam culpa expedita laudantium excepturi harum iure odio atque consequuntur delectus. Explicabo vero, nobis hic minus similique voluptates quo!</p>
+				<p className="firstp">This visual interface, developed by UC Berkeley in collaboration with Tec de Monterrey, uses open data sources from CENACE and is meant to serve as a support for researchers, and entities interested in the energy sector in M&eacute;xico. National system demand, and local marginal prices can be both observed, compared, and downloaded. </p>
+				<p>For inquiries please reach out to <a style={{color: '#4CF1CD'}} href="mailto:sergioc@berkeley.edu">sergioc@berkeley.edu</a></p>
 				<div className="demandaprecio">
-					<ul>
-						<li><a name="demanda" onClick={this.toggleView} className={this.state.active === 1 ? 'active' : ''} href="#">Demanda</a></li>
-						<li><a name="precios" onClick={this.toggleView} className={this.state.active === 2 ? 'active' : ''} href="#">Precios</a></li>
-						<li><a name="datos" onClick={this.toggleView} className={this.state.active === 3 ? 'active' : ''} href="#">Datos</a></li>
+					<ul className="nav-tabs">
+						<li><a name="demanda" onClick={this.toggleView} className={this.state.active === 1 ? 'active' : ''} href="#">Day Ahead Market</a></li>
+						<li><a name="precios" onClick={this.toggleView} className={this.state.active === 2 ? 'active' : ''} href="#">Local Marginal Prices</a></li>
+						<li><a name="datos" onClick={this.toggleView} className={this.state.active === 3 ? 'active' : ''} href="#">Download Data</a></li>
 					</ul>
 					<hr/>
 				</div>
@@ -49,7 +50,22 @@ class App extends Component {
 				<Precios visible={this.state.active === 2} />
 				<DatosFrame visible={this.state.active === 3} />
 
-				<p className="footer" style={{textAlign: 'center', marginTop: '60px'}}>2018 &copy; Berkeley Energy and Climate Institute</p>
+				<p className="footer" style={{textAlign: 'center', width: '50%', margin: '60px auto 0'}}>
+					2018 &copy; California Institute of Energy and Environment 
+					<br /> 
+					<small><em>through its collaborative work with Tec de Monterrey under the 'Binational Laboratory on Smart Sustainable Energy Management and Technology Training'</em></small>
+				</p>
+				<section className="footer-logos">
+					<div className="col">
+						<img src="/berkeley.svg" alt="University of California"/>
+					</div>
+					<div className="col">
+						<img src="/uciee.svg" alt="California Institute of Energy and Environment"/>
+					</div>
+					<div className="col">
+						<img src="/tec.svg" alt="Tecn&oacute;logico de Monterrey"/>
+					</div>
+				</section>
 			</div>
 		);
 	}
